60c0dd81770265a892cf06fe7f6c7be6dd962ce6
Marco <juniorlograjeda@gmail.com>
2016-06-29 17:34:14 -0600

    Fragments

---
+++ app/src/main/java/org/marco/tipcalc/adapters/TipAdapter.java
@@ -0,0 +1,7 @@
+package org.marco.tipcalc.adapters;
+
+/**
+ * Created by marco on 6/29/16.
+ */
+public class TipAdapter {
+}
---
+++ app/src/main/java/org/marco/tipcalc/models/TipRecord.java
@@ -0,0 +1,7 @@
+package org.marco.tipcalc.models;
+
+/**
+ * Created by marco on 6/29/16.
+ */
+public class TipRecord {
+}
